fcd44af8bd748e2e4ac9ca030d84638971db0c0f
Jonathan Harlan <jjharlan06@gmail.com>
2016-10-18 14:05:07 -0400

    Connect TableViewController

---
+++ DreamLister/ItemCell.swift
@@ -0,0 +1,18 @@
+//
+//  ItemCell.swift
+//  DreamLister
+//
+//  Created by JONATHAN HARLAN on 10/18/16.
+//  Copyright © 2016 JONATHAN HARLAN. All rights reserved.
+//
+
+import UIKit
+
+class ItemCell: UITableViewCell {
+
+    @IBOutlet weak var thumb: UIImageView!
+    @IBOutlet weak var title: UILabel!
+    @IBOutlet weak var price: UILabel!
+    @IBOutlet weak var details: UILabel!
+    
+}
